9d18268dfeffd2f909e85edbe75ecde4c703079f
EchoCow <lizhongyue248@163.com>
2019-06-18 22:05:30 +0800

    :tada: Initial commit.

---
+++ src/main/java/cn/edu/gzmu/resource/TestController.java
@@ -0,0 +1,24 @@
+package cn.edu.gzmu.resource;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+/**
+ * 测试
+ *
+ * @author echo
+ * @date 19-6-18 上午11:45
+ */
+@RestController
+public class TestController {
+    @GetMapping("/user")
+    public HttpEntity<?> userInfo(Principal principal) {
+        return ResponseEntity.ok(principal);
+    }
+}
---
+++ src/main/java/cn/edu/gzmu/resource/config/OAuth2ResourceServerConfig.java
@@ -0,0 +1,107 @@
+package cn.edu.gzmu.resource.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.jwt.crypto.sign.RsaVerifier;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Map;
+
+/**
+ * @author echo
+ * @date 19-6-18 下午1:21
+ */
+@Configuration
+@EnableResourceServer
+@EnableOAuth2Client
+public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    private final ResourceServerProperties resourceServerProperties;
+
+    private static final String DEMO_RESOURCE_ID = "resource";
+
+    public OAuth2ResourceServerConfig(ResourceServerProperties resourceServerProperties) {
+        this.resourceServerProperties = resourceServerProperties;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(DEMO_RESOURCE_ID).stateless(true)
+                .tokenServices(tokenServices());
+    }
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/user/**")
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//        converter.setVerifierKey(getPubKey());
+        converter.setSigningKey("EchoCow");
+        return converter;
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        return defaultTokenServices;
+    }
+
+    private String getPubKey() {
+       return StringUtils.isEmpty(resourceServerProperties.getJwt().getKeyValue())
+               ? getKeyFromAuthorizationServer()
+               : resourceServerProperties.getJwt().getKeyValue();
+    }
+
+    private String getKeyFromAuthorizationServer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", encodeClient());
+        HttpEntity<String> requestEntity = new HttpEntity<>(null, httpHeaders);
+        String pubKey = new RestTemplate()
+                .getForObject(resourceServerProperties.getJwt().getKeyUri(), String.class, requestEntity);
+        try {
+            Map map = objectMapper.readValue(pubKey, Map.class);
+            System.out.println("联网公钥");
+            return map.get("value").toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String encodeClient() {
+        return "Basic " + Base64.getEncoder().encodeToString((resourceServerProperties.getClientId()
+                + ":" + resourceServerProperties.getClientSecret()).getBytes());
+    }
+}
